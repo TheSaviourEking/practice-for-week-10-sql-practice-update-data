1a3c3bf11c79d4e44a69d6dbcf3a3026e00c75d7
TheSaviourEking <saviour.eking@outlook.com>
2024-02-21 04:01:39 +0100

    chore: UPDATE DATA Bonus

--- sql-practice/my-queries.sql
+++ sql-practice/my-queries.sql
@@ -8,14 +8,13 @@ WHERE
     AND last_name = 'Pond';
 
 -- Step 2: Silent failure 
-UPDATE
-    friends
-SET
-    last_name = 'smith'
-WHERE
-    first_name = 'Tyler'
-    AND last_name = 'Rose';
-
+-- UPDATE
+--     friends
+-- SET
+--     last_name = 'smith'
+-- WHERE
+--     first_name = 'Tyler'
+--     AND last_name = 'Rose';
 -- Step 2 FIX
 UPDATE
     friends
@@ -24,6 +23,28 @@ SET
 WHERE
     first_name = 'Rose'
     AND last_name = 'Tyler';
+
+-- Trigger an UPDATE error
+-- UPDATE
+--     friends
+-- SET
+--     last_name = NULL
+-- WHERE
+--     id = 5;
+
+-- what error do you see ?
+-- Error: near line 28: stepping, NOT NULL constraint failed: friends.last_name (19)
+-- why does this error happen ?
+-- it happens because in the db schema we did set a constraint that last_name cannot be NULL;
+-- Resolve the error
+-- i'll set the last_name to an empty string instead, this adheres to the constraint
+-- that last_name cannot be null and also removes the last_name from the friends table
+UPDATE
+    friends
+SET
+    last_name = ''
+WHERE
+    id = 5;
 
 -- Verify table
 SELECT
